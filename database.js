a3611a3b4d3b93e0739b750e5938bb7094102bf3
DillonCarpenter <165092222+DillonCarpenter@users.noreply.github.com>
2025-05-02 19:58:41 -0400

    Update Dillon Branch to current version (#9)
    * Dillon (#5)
    
    * Got Axios to work. GET Requests are good!
    
    * Added Coordinates to battleship.
    
    * Created voting System for Battleship. It is very primitive!
    
    * Updated Render logic to account for pending, hit, and miss statuses.
    
    * applyed template
    
    * minor fixes to new template
    
    * minor adjustments
    
    * Implemented sign in
    
    * Tried moving sign-in
    
    * Update ReadME file.
    
    * added video link to README
    
    * delete useless image
    
    * added database access - authentication fails
    
    * retreived data from database
    
    * updated so getting collection can be imported
    
    ---------
    
    Co-authored-by: FallingCar <drewbug722@gmail.com>
    Co-authored-by: David-Michael Davies <foreverreign45@gmail.com>
    Co-authored-by: Chatroom64 <61385406+Chatroom64@users.noreply.github.com>
    Co-authored-by: Drew Hanson <46378250+FallingCar@users.noreply.github.com>

---
+++ database.js
@@ -0,0 +1,40 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Setup
+const uri = "mongodb+srv://general:general@gamehostcluster.g3wicus.mongodb.net/?retryWrites=true&w=majority&appName=GameHostCluster";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let db;
+
+// Connect to the database
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db("ASE220GameHosterDB");
+    console.log("Connected to DB");
+  } catch (err) {
+    console.error("Failed to connect to DB:", err);
+  }
+}
+
+// Fetch data from a collection
+async function getCollectionData(collectionName) {
+  try {
+    const collection = db.collection(collectionName);
+    const data = await collection.find().toArray();
+    return data;
+  } catch (err) {
+    console.error("Failed to fetch data:", err);
+    throw err;
+  }
+}
+
+connectDB();
+
+module.exports = { getCollectionData };
