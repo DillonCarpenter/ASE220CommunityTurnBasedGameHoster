d5d343232c9f2c63dd9926ec34a03e0ab19f07e4
DillonCarpenter <dillwilldwc@gmail.com>
2025-05-02 23:39:19 -0400

    See previous commit.

--- server.js
+++ server.js
@@ -14,6 +14,38 @@ app.get('/', async (req, res) => {
   }
 });
 
+app.get('/api/user/:userID', async (req, res) => {
+  const userID = req.params.userID;
+
+  //Access the db and get the correct user document
+});
+
+//User endpoints
+app.put('/api/user/:userID', async (req, res) => {
+  const userID = req.params.userID;
+
+  //Grab the response body and use it to update the user document
+});
+
+app.post('/api/user/create', async (req, res) => {
+  //Create a new user document for a user that just created an account
+  //Should probably verify that the account was successfully created as well
+});
+
+//Battleship Endpoints
+
+app.get('/api/Battleship/:gameID', async (req, res) => {
+  const gameID = req.params.gameID;
+  //Get the Correct Battleship document
+});
+
+app.put('/api/Battleship/:gameID', async (req, res) => {
+  const gameID = req.params.gameID;
+  //Intended to update a Battleship document
+});
+
+app.post('/api/Battleship', async (req, res) => {
+});
 // Start the server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
