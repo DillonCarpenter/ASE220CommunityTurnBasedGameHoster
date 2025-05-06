ec711c8092641f0fb78580d61f767640624ec1b2
FallingCar <drewbug722@gmail.com>
2025-05-06 16:48:39 -0400

    Voting works, started making timer, fixed post endpoint

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ app.use(express.json());
 connectDB().then(() => {
   app.listen(3000, () => {
     console.log("Server is running on port 3000");
+    while(gameWon == 0){
+      let seconds = 33;
+      const countdownElement = document.getElementById('countdown');
+      const countdown = setInterval(() => {
+          seconds--;
+          if(seconds >= 3) {
+            countdownElement.textContent = (seconds-3);
+          }
+          if (seconds < 3) {
+              clearInterval(countdown);
+              countdownElement.textContent = "Time's up!";
+          }
+          if(seconds < 0) {
+            // timer logic goes here to choose most voted move, and to choose opponent move
+          }
+      }, 1000);
+    }
   });
 }).catch(err => {
   console.error("Failed to connect to DB:", err);
@@ -66,7 +83,15 @@ app.get('/api/Battleship/:gameID', async (req, res) => {
 
 app.post('/api/Battleship/:gameID/votes', async (req, res) => {
   const gameID = req.params.gameID;
-  //Intended to update a Battleship document with the new vote.
+  const { coordinate } = req.body;
+  const db = getDB();
+  const game = await db.collection('BattleShipGames').findOne({ gameID });
+  const enemyBoard = game.EnemyBoard;
+  enemyBoard[coordinate].votes += 1;
+  await db.collection('BattleShipGames').updateOne(
+    { gameID },
+    { $set: { EnemyBoard: enemyBoard } }
+  );
 });
 
 app.post('/api/Battleship/new', async (req, res) => {
